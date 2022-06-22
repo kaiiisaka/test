8d5c9cccb740a88935c80bd041e3b933b6503379
Alex <panches2001@gmail.com>
2022-06-22 05:16:45 +0300

    Some SCC

--- scripts.js
+++ scripts.js
@@ -7,4 +7,15 @@
     
     //alert(user.name); // 'Петя', изменения видны по ссылке из переменной "user"
 
+    let user1 = {
+        name1: "Джон",
+        age1: 30
+      };
+      
+      user1.sayHi = function() {
+        alert("Привет!");
+      };
+      
+      user1.sayHi(); // Привет!
+
 
